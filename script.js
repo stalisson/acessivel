8e3785f144d6ada5cf5d0490638fc18e9914a4ab
Istalisson <149580590+stalisson@users.noreply.github.com>
2025-03-20 20:33:41 -0300

    a

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ document.addEventListener('DOMContentLoaded', function(){
  })
  
  ScrollReveal().reveal('#inicio', { delay: 500 });
- ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ ScrollReveal().reveal('#yamaha', { delay: 500 });
  ScrollReveal().reveal('#galeria', { delay: 500 });
- ScrollReveal().reveal('#contato', { delay: 500 });
+ ScrollReveal().reveal('#info', { delay: 500 });
+ ScrollReveal().reveal('#refe', { delay: 500 });
